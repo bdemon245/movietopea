ae86e1783e515dda60d03659ed87396671f15616
Mojahidul Islam <bdemon245@gmail.com>
2022-12-25 22:30:19 +0600

    added new api, placeholder and pagination option

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -6,59 +6,184 @@ import '../css/search-bar.css'
 
 
 function App() {
-  const movie = {
-    "Title": "Avatar",
-    "Year": "2009",
-    "imdbID": "tt0499549",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_SX300.jpg"
-  }
-  const API_URL = 'http://www.omdbapi.com/?apikey=30f746b0'
+
+  // const movie = {
+  //   "adult": false,
+  //   "backdrop_path": "/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg",
+  //   "id": 76600,
+  //   "title": "Avatar: The Way of Water",
+  //   "original_language": "en",
+  //   "original_title": "Avatar: The Way of Water",
+  //   "overview": "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  //   "poster_path": "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
+  //   "media_type": "movie",
+  //   "genre_ids": [
+  //     878,
+  //     12,
+  //     14,
+  //     28
+  //   ],
+  //   "popularity": 8054.532,
+  //   "release_date": "2022-12-14",
+  //   "video": false,
+  //   "vote_average": 8.059,
+  //   "vote_count": 1166
+  // }
+  const trending_url = 'https://api.themoviedb.org/3/trending/all/week?api_key='
+  const API_KEY = "04869aa452b9abeb3d6489bcbae7a5ee"
+  const search_url = 'https://api.themoviedb.org/3/search/multi?api_key='
+  const array = new Array(20)
+
   const [movies, setMovies] = useState([])
-  const [query, setQuery] = useState('')
-  const [title, setTitle] = useState('')
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`)
-    const data = await response.json()
-    setMovies(data.Search)
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+  const [title, setTitle] = useState(search)
+  const [isSearching, setIsSearching] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+
+  const [query, setQuery] = useState(`${trending_url + API_KEY}&page=${page}`)
+  const searchMovies = (query) => {
+    setIsLoading(true)
+    fetch(query)
+      .then(res => res.json())
+      .then(data => {
+        setMovies(data.results)
+        setIsLoading(false)
+      })
   }
   useEffect(() => {
-    searchMovies(title)
+    searchMovies(query)
 
-  }, [title])
+  }, [query])
+
+
+  // a loop for printing placeholders
+  function printPlaceholder(times) {
+    for (let i = 0; i < times; i++) {
+      <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+        <MovieCard movie='isLoading' />
+      </div>
+    }
+  }
   return (
     <div className="App w-100 h-100">
       <Header />
       <div className='container px-5 py-2 d-flex justify-content-center'>
         <div id="search-bar" className='d-flex align-items-center' style={{ width: '80%' }}>
           <input
-          style={{fontSize: 16}}
+            style={{ fontSize: 16 }}
             className="form-control me-2 bg-dark search-box text-secondary"
             placeholder="Search for movies"
-            value={query}
+            value={search}
             onChange={(e) => {
-              setQuery(e.target.value)
+              setSearch(e.target.value)
             }} aria-label="Search" />
-          <button className='bg-none p-2 rounded-3 search-btn text-center' onClick={() => setTitle(query)} onSubmit={() => setTitle(query)}> <i
+          <button className='bg-none p-2 rounded-3 search-btn text-center' onClick={() => {
+            setQuery(`${search_url + API_KEY}&page=${page}&query=${search}`)
+            setIsSearching(true)
+            setTitle(search)
+          }}> <i
             className="fa-solid fa-search" style={{ fontSize: 16 }}></i></button>
 
         </div>
       </div>
-      <div className="row">
-        {
-          movies?.length > 0 ? (
-            movies.map(movie => (
-              <div className="col-sm-3 d-flex justify-content-center align-items-center">
-                <MovieCard movie={movie} key={movie.imdbID} />
+      {/* movies panel */}
+      <div id='trending-section' className="container">
+        <h2 className="text-light">{isSearching === true ? `Showing results for "${title}"` : "Trendig"}</h2>
+        <div className='row'>
+          {
+
+            isLoading === true ? <>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
               </div>
-            ))
-          ) : (
-            <div className="text-light text-center">
-              Loading...
-            </div>
-          )
-        }
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+              <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <MovieCard movie='isLoading' />
+              </div>
+
+            </> :
+              movies.map(movie =>
+              (
+                <div className="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                  <MovieCard movie={movie} key={movie.id + "@" + movie.release_date} />
+                </div>
+              ))
+          }
+        </div>
       </div>
+
+      {/* pagination */}
+      <nav className='d-flex justify-content-center' aria-label="...">
+        <ul className="pagination">
+          <li className="page-item disabled">
+            <span className="page-link" href="#">Previous</span>
+          </li>
+          <li className="page-item active" aria-current="page"><span className="page-link" href="#">1</span></li>
+          <li className="page-item" onClick={() => {
+            setPage(page + 1)
+            setQuery(`${trending_url + API_KEY}&page=${page}`)
+            console.log(page)
+          }}>
+            <span className="page-link" href="#">Next</span>
+          </li>
+        </ul>
+      </nav>
+
     </div>
   )
 }
--- src/js/MovieCard.jsx
+++ src/js/MovieCard.jsx
@@ -1,16 +1,37 @@
 import '../css/card.css'
 function MovieCard({ movie }) {
+    const isLoading = movie === 'isLoading'
+    const title = movie.title === undefined ? movie.name : movie.title
+    const poster = 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+    const release_date = movie.release_date === undefined ? movie.first_air_date : movie.release_date
+    const { media_type, overview, vote_average, vote_count } = movie
 
     return (
-        <div className="movie-card m-3">
-            <img src={movie.Poster === 'N/A' ? "https://via.placeholder.com/166" : movie.Poster } alt={movie.Title} />
-            <div className="movie-info">
-                <div className="wrapper">
-                    <h3 className="movie-title">{movie.Title}</h3>
-                    <div className='movie-details'>
-                        <p className="movie-subtitle">{movie.Type.toUpperCase()}</p>
-                        <p className="movie-year">{movie.Year}</p>
+        <div className={isLoading ? "movie-card m-3 placeholder-glow" : "movie-card m-3"}>
+            <div className="hidden-div movie-details">
+                <h3 className={isLoading ? 'placeholder col-6' : "movie-title mb-3"}>{title}</h3>
+                <div className="d-flex justify-content-between">
+                    <div className='details justify-self-start'>
+                        <p className='' style={{ margin: 0 }}>Type: <br /><span className='ms-2' >{media_type === undefined ? undefined : media_type.toUpperCase()}</span> </p>
+                        <p className='' style={{ margin: 0 }}>Release:<br /><span className='ms-2' >{release_date}</span></p>
                     </div>
+                    <div className='details justify-self-end'>
+                        <p style={{ marginBottom: 5 }} className="text-end">
+                            <span>{`(${vote_average})`}</span>
+                            <i className="fa-solid fa-star"></i>
+                        </p>
+                        <p className="text-end">
+                            <span>{`(${vote_count})`}</span>
+                            <i className="fa-solid fa-user"></i>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <img className={isLoading && "placeholder col-12"} src={movie.poster_path === undefined ? "http://via.placeholder.com/500" : poster} alt={title} />
+            <div className="movie-info">
+                <div className={isLoading ? 'placeholder-glow' : ''}>
+                    <h3 className={isLoading ? 'placeholder col-6' : "movie-title"}>{title}</h3>
                 </div>
             </div>
         </div>
